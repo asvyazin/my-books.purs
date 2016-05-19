b11f404917865d31024a339a2103df902260504e
Alexander Svyazin <guybrush@live.ru>
2016-05-19 07:24:33 +0500

    fix JSON contract

--- server/BookIndexer/Onedrive/AudioFacet.hs
+++ server/BookIndexer/Onedrive/AudioFacet.hs
@@ -2,34 +2,50 @@
 module BookIndexer.Onedrive.AudioFacet where
 
 
-import Data.Aeson (FromJSON(parseJSON), Value(Object), (.:))
+import Data.Aeson (FromJSON(parseJSON), Value(Object), (.:), (.:?))
 import Data.Int (Int64)
 import Data.Text (Text)
 
 
 data AudioFacet =
   AudioFacet
-  { audioFacetAlbum :: Text
-  , audioFacetAlbumArtist :: Text
-  , audioFacetArtist :: Text
-  , audioFacetBitrate :: Int
-  , audioFacetComposers :: Text
-  , audioFacetCopyright :: Text
-  , audioFacetDisk :: Int
-  , audioFacetDiskCount :: Int
+  { audioFacetAlbum :: Maybe Text
+  , audioFacetAlbumArtist :: Maybe Text
+  , audioFacetArtist :: Maybe Text
+  , audioFacetBitrate :: Maybe Int
+  , audioFacetComposers :: Maybe Text
+  , audioFacetCopyright :: Maybe Text
+  , audioFacetDisk :: Maybe Int
+  , audioFacetDiskCount :: Maybe Int
   , audioFacetDuration :: Int64
-  , audioFacetGenre :: Text
+  , audioFacetGenre :: Maybe Text
   , audioFacetHasDrm :: Bool
   , audioFacetIsVariableBitrate :: Bool
-  , audioFacetTitle :: Text
-  , audioFacetTrack :: Int
-  , audioFacetTrackCount :: Int
-  , audioFacetYear :: Int
+  , audioFacetTitle :: Maybe Text
+  , audioFacetTrack :: Maybe Int
+  , audioFacetTrackCount :: Maybe Int
+  , audioFacetYear :: Maybe Int
   } deriving (Show)
 
 
 instance FromJSON AudioFacet where
   parseJSON (Object o) =
-    AudioFacet <$> o .: "album" <*> o .: "albumArtist" <*> o .: "artist" <*> o .: "bitrate" <*> o .: "composers" <*> o .: "copyright" <*> o .: "disk" <*> o .: "diskCount" <*> o .: "duration" <*> o .: "genre" <*> o .: "hasDrm" <*> o .: "isVariableBitrate" <*> o .: "title" <*> o .: "track" <*> o .: "trackCount" <*> o .: "year"
+    AudioFacet <$>
+    o .:? "album" <*>
+    o .:? "albumArtist" <*>
+    o .:? "artist" <*>
+    o .:? "bitrate" <*>
+    o .:? "composers" <*>
+    o .:? "copyright" <*>
+    o .:? "disk" <*>
+    o .:? "diskCount" <*>
+    o .: "duration" <*>
+    o .:? "genre" <*>
+    o .: "hasDrm" <*>
+    o .: "isVariableBitrate" <*>
+    o .:? "title" <*>
+    o .:? "track" <*>
+    o .:? "trackCount" <*>
+    o .:? "year"
   parseJSON _ =
     error "Invalid AudioFacet JSON"
